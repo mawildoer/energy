8fb28ec5e53da327083d010752696dbf9f1859d5
mawildoer <mawildoer@gmail.com>
2021-05-01 12:40:34 +0200

    Add intial content

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Energy
 
 This repository contains global energy data and python hooks to easily manipulate that data.
+
+Uses [Git LFS](https://git-lfs.github.com/) for tracking *.csv files. 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="energy",
+    version="0.1",
+    author="",
+    license="GPLv3",
+    packages=["energy"],
+    install_requires=[""],
+    package_data={"energy": ["*.tif", "*.csv", "*.yml"]}
+)
